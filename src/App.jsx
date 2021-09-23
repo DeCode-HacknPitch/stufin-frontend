38166bd163b60f60379f74298a0d188195783bb8
Sayan <bhattacharya.sayan30@gmail.com>
2021-09-23 22:27:28 +0530

    mui done

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,10 @@
+ import Button from '@mui/material/Button';
+
+
 const App = () => {
-  return <div></div>;
+  return <div>
+    <Button variant="contained">Hello World</Button>
+  </div>;
 };
 
 export default App;
