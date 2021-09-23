e5d3cb334ae43961d21e25aa9f7f3cdff69a372e
Sayan <bhattacharya.sayan30@gmail.com>
2021-09-23 22:33:33 +0530

    firebase config

---
+++ src/config.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDDTNzUOXR88_l_tSNG-GgixTkJFoeqt7Y",
+  authDomain: "stufin-d35b6.firebaseapp.com",
+  projectId: "stufin-d35b6",
+  storageBucket: "stufin-d35b6.appspot.com",
+  messagingSenderId: "618618542342",
+  appId: "1:618618542342:web:db25312e38611d05682687",
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
